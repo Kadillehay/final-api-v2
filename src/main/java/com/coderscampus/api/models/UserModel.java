dda1e802ef3f89a6e15d40ff721f6f98db9c1ff6
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-10-26 13:26:46 -0400

    added new method to update tuser

--- src/main/java/com/coderscampus/api/models/UserModel.java
+++ src/main/java/com/coderscampus/api/models/UserModel.java
@@ -2,27 +2,67 @@ package com.coderscampus.api.models;
 
 public class UserModel {
 
-	
-	
 	private String emailAddress;
 	private String password;
+	private String originalEmail;
+	private String originalPassword;
+	private String farmName;
+	private String phoneNumber;
+
+	public String getFarmName() {
+		return farmName;
+	}
+
+	public void setFarmName(String farmName) {
+		this.farmName = farmName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getOriginalEmail() {
+		return originalEmail;
+	}
+
+	public void setOriginalEmail(String originalEmail) {
+		this.originalEmail = originalEmail;
+	}
+
+	public String getOriginalPassword() {
+		return originalPassword;
+	}
+
+	public void setOriginalPassword(String originalPassword) {
+		this.originalPassword = originalPassword;
+	}
+
 	public String getEmailAddress() {
 		return emailAddress;
 	}
+
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	@Override
 	public String toString() {
-		return "UserModel [emailAddress=" + emailAddress + ", password=" + password + "]";
+		return "UserModel [emailAddress=" + emailAddress + ", password=" + password + ", originalEmail=" + originalEmail
+				+ ", originalPassword=" + originalPassword + ", farmName=" + farmName + ", phoneNumber=" + phoneNumber
+				+ "]";
 	}
-	
-	
-	
+
+
 }
--- src/main/java/com/coderscampus/api/web/DashboardController.java
+++ src/main/java/com/coderscampus/api/web/DashboardController.java
@@ -1,37 +1,54 @@
-//package com.coderscampus.api.web;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.BeanUtils;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.coderscampus.api.domain.User;
-//import com.coderscampus.api.models.UserModel;
-//import com.coderscampus.api.repository.FarmRegisterRepository;
-//
-//@RestController
-//@CrossOrigin(origins = "*")
-//public class DashboardController {
-//	
-//	@Autowired
-//	FarmRegisterRepository farmRegisterRepo;
-//	@Autowired
-//	UserModel userModel;
-//	
-//	//Ogul I just copied this from the register, I assume the logic is the same?
-//	@PostMapping("/user-dashboard")
-//	public ResponseEntity<List<Object>> updateRegister(@RequestBody User farmRegistry) {
-//		User user = new User();
-//		BeanUtils.copyProperties(farmRegistry, user);
-//		
-//		User registered = farmRegisterRepo.save(user);
-//		System.out.println("working?");
-//		return ResponseEntity.ok(List.of(registered.getId(), registered.getFarmName()));
-//	
-//	}
-//}
+package com.coderscampus.api.web;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coderscampus.api.domain.User;
+import com.coderscampus.api.models.UserModel;
+import com.coderscampus.api.repository.FarmRegisterRepository;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class DashboardController {
+
+	@Autowired
+	FarmRegisterRepository farmRegisterRepo;
+
+	// Ogul I just copied this from the register, I assume the logic is the same?
+	@PostMapping("/user-dashboard")
+	public ResponseEntity<List<Object>> updateRegister(@RequestBody User user) {
+		System.out.println(user);
+		User newUser = new User();
+		BeanUtils.copyProperties(user, newUser);
+
+		User registered = farmRegisterRepo.save(newUser);
+		System.out.println("working?");
+		return ResponseEntity.ok(List.of(registered.getId(), registered.getFarmName()));
+
+	}
+
+	@PostMapping("/update-user")
+	public ResponseEntity<User> updateUser(@RequestBody UserModel user) {
+		System.out.println(user);
+		User foundUser = farmRegisterRepo.findUserByEmailAndPassword(user.getOriginalEmail(), user.getOriginalPassword());
+		User updatedUser = null;
+		if(foundUser.getEmailAddress() != null) {
+			
+	
+			foundUser.setFarmName(user.getFarmName());
+			foundUser.setEmailAddress(user.getEmailAddress());
+			foundUser.setPassword(user.getPassword());
+			foundUser.setPhoneNumber(user.getPhoneNumber());
+			updatedUser = farmRegisterRepo.save(foundUser);
+		}
+
+		return ResponseEntity.ok(updatedUser);
+	}
+}
